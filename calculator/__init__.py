147da7603a66603f271e8f393965a24de65adb6c
fa328 <fa328@njit.edu>
2025-02-10 10:30:53 -0500

    Update calculator

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,17 +1,17 @@
 """My level 3 Calcu"""
 
 def add(a, b):
-    """Function printing python version."""
+    """Test that addition function works."""
     return a + b
 
 def subtract(a, b):
-    """Function printing python version."""
+    """Test that subtraction function works"""
     return a - b
 
 def multiply(a, b):
-    """Function printing python version."""
+    """Test that multiply fuction works"""
     return a * b
 
 def divide(a, b):
-    """Function printing python version."""
+    """Test that divide fuction works"""
     return a / b
