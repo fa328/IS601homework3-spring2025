0f574d5a3a4a127c240fd9a54b2d2db38e17a51b
fa328 <fa328@njit.edu>
2025-02-10 12:28:59 -0500

    update calculator init

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,45 +1,21 @@
+from calculator.calculation import Calculation
+from calculator.operation import add, subtract, multiply, divide
+
 class Calculator:
     @staticmethod
-    def add(a, b):
-        return a + b
-    
+    def add(a,b):
+        calculation = Calculation(a, b, add) # add function
+        return calculation.get_result()
     @staticmethod
-    def subtract(a, b):
-        return a - b
-    
+    def subtract(a,b):
+        Calculation = Calculation(a, b, subtract) # subtract function
+        return calculation.get_result()
     @staticmethod
-    def multiply(a, b):
-        return a * b
-    
+    def multiply (a,b):
+        Calculation = Calculation(a, b, multiply) # multiply fuction
+        return calculation.get_result()
     @staticmethod
-    def divide(a, b):
-        if b == 0:
-            return "Error: Division by zero"
-        return a / b
+    def divide (a,b):
+        Calculation = Calculation(a, b, divide) # divide fuction
+        return calculation.get_result()
 
-
-class Calculation:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
-        self.result = None
-
-    def perform_addition(self):
-        self.result = Calculator.add(self.num1, self.num2)
-        return self.result
-
-    def perform_subtraction(self):
-        self.result = Calculator.subtract(self.num1, self.num2)
-        return self.result
-
-    def perform_multiplication(self):
-        self.result = Calculator.multiply(self.num1, self.num2)
-        return self.result
-
-    def perform_division(self):
-        self.result = Calculator.divide(self.num1, self.num2)
-        return self.result
-
-    def get_result(self):
-        return self.result
-    
