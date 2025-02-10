396ad9df979bc089613d03100d1c6a54468ebbd6
fa328 <fa328@njit.edu>
2025-02-10 14:36:19 -0500

    add test calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -9,10 +9,10 @@ def test_subtraction():
     '''Test that subtract function works '''    
     assert Calculator.subtract(2,2) == 0
 
-def test_multiplication():
+def test_multipy():
     '''Test that multiply fuction works'''
     assert Calculator.multiply(2,2) == 4
 
-def test_division():
+def test_divide():
     '''Test that division fuction works'''
     assert Calculator.divide(2,2) == 1
