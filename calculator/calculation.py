63b50c9adb95e4d2a1ed343d7179db750b480a47
fa328 <fa328@njit.edu>
2025-02-10 17:45:40 -0500

    Update calculation

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -1,10 +1,11 @@
-class Calculations:
-    """Encapsulates a single calculation."""
+'''Calculations Class functions'''
+class Calculations: # pylint: disable=too-few-public-methods
+    '''Calculations functions'''
     def __init__(self, a, b, operation):
         self.a = a
         self.b = b
         self.operation = operation
 
     def get_result(self):
-        # call
+        '''Get result functions'''
         return self.operation(self.a, self.b)
