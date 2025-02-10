84f540b97eb2cd8114b40fc58eb6551fcfde1b9e
fa328 <fa328@njit.edu>
2025-02-10 01:09:02 -0500

    update calculator file

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,11 +1,15 @@
 def add(a, b):
+    '''Test that addition function works '''    
     return a + b
 
 def subtract(a, b):
+     '''Test that subtract function works '''    
     return a - b
 
 def multiply(a, b):
+    '''Test that multiply fuction works '''
     return a * b
 
 def divide(a, b):
+    '''Test that division fuction works '''
     return a / b
