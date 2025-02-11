5593198d61227e9b9265bca184f747eb56825699
fa328 <fa328@njit.edu>
2025-02-10 20:32:14 -0500

    Update test calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,19 +1,19 @@
 '''My Calculator Test'''
-from calculator import Calculator
-
+from calculator.operations import add, subtract, multiply, divide
 
 def test_addition():
     '''Test that addition function works '''    
-    assert Calculator.add(2,2) == 4
+    assert add(2,2) == 4
 
 def test_subtraction():
     '''Test that subtract function works '''    
-    assert Calculator.subtract(2,2) == 0
+    assert subtract(2,2) == 0
 
-def test_multipy():
+def test_multiplication():
     '''Test that multiply fuction works'''
-    assert Calculator.multiply(2,2) == 4
+    assert multiply(2,2) == 4
 
-def test_divide():
+def test_division():
     '''Test that division fuction works'''
-    assert Calculator.divide(2,2) == 1
+    assert divide(2,2) == 1
+
