0d6ebf9da430e4ccfcc1a27fb52e7915c97d855c
fa328 <fa328@njit.edu>
2025-02-13 21:01:26 -0500

    Update

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -21,7 +21,11 @@ def divide(a, b):
     return a / b
 =======
 '''My Calculator Test'''
+<<<<<<< HEAD
 >>>>>>> Main
+=======
+import pytest # type: ignore
+>>>>>>> part3
 from calculator.calculation import Calculations
 from calculator.operations import add, subtract, multiply, divide
 
@@ -51,25 +55,25 @@ class Calculator:
 =======
 
     @staticmethod
-    def add(a: float, b: float) -> str:
+    def add(a: float, b: float) -> float:
         '''Test that addition function works '''    
         calculation = Calculations(a, b, add)
         return calculation.get_result()
 
     @staticmethod
-    def subtract(a: float, b: float) -> str:
+    def subtract(a: float, b: float) -> float:
         '''Test that subtract function works '''    
         calculation = Calculations(a, b, subtract)
         return calculation.get_result()
 
     @staticmethod
-    def multiply(a: float, b: float) -> str:
+    def multiply(a: float, b: float) -> float:
         '''Test that multiply function works '''    
         calculation = Calculations(a, b, multiply)
         return calculation.get_result()
 
     @staticmethod
-    def divide(a: float, b: float) -> str:
+    def divide(a: float, b: float) -> float:
         '''Test that divide function works '''    
         if b == 0:
             return "Error: Division by zero"
@@ -80,7 +84,7 @@ class CalculationsHistory:
     history = []
 
     @classmethod
-    def add_history(cls, calculation):
+    def add_history(cls, calculation: float):
         '''Performs addition, subtraction, multiplication, division'''
         cls.history.append(calculation)
 
@@ -93,5 +97,12 @@ class CalculationsHistory:
     def clear_history(cls):
         '''Use to clears the history'''
         cls.history = []
+<<<<<<< HEAD
+
+calc = Calculator()
+result = calc.add(2, 2)
+CalculationsHistory.add_history(result)
+print(CalculationsHistory.get_history())
+=======
 >>>>>>> part3
 >>>>>>> Main
